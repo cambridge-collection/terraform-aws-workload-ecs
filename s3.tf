c40c90df4655c12c54ed102dc24cac3dbe1d87a8
Rory McHugh <rmm98@cam.ac.uk>
2024-06-10 16:50:25 +0100

    fix(s3): Fix error with for_each loop on aws_s3_object resources
    Fix error "Invalid for_each argument: var.s3_task_execution_bucket
    is a string, known only after apply"

--- s3.tf
+++ s3.tf
@@ -1,13 +1,13 @@
 # NOTE Terraform will throw an error if a sensitive variable is used directly in for_each
 resource "aws_s3_object" "task_execution" {
-  for_each = var.s3_task_execution_bucket != null ? nonsensitive(toset(keys(var.s3_task_execution_bucket_objects))) : toset([])
+  for_each = nonsensitive(toset(keys(var.s3_task_execution_bucket_objects)))
   bucket   = var.s3_task_execution_bucket
   key      = each.key
   content  = var.s3_task_execution_bucket_objects[each.key]
 }
 
 resource "aws_s3_object" "task" {
-  for_each = var.s3_task_bucket != null ? nonsensitive(toset(keys(var.s3_task_bucket_objects))) : toset([])
+  for_each = nonsensitive(toset(keys(var.s3_task_bucket_objects)))
   bucket   = var.s3_task_bucket
   key      = each.key
   content  = var.s3_task_bucket_objects[each.key]
