33337ec1fe13209c4c1a1f49943a7554b7c53f1f
Rory McHugh <rmm98@cam.ac.uk>
2024-07-09 15:02:24 +0100

    fix(locals): Prevent error with coalescelist function where there are no non-null arguments

--- locals.tf
+++ locals.tf
@@ -7,5 +7,5 @@ locals {
   s3_task_execution_bucket_arns            = concat(compact([local.s3_task_execution_bucket_arn]), local.s3_task_execution_additional_bucket_arns)
   s3_task_execution_bucket_arns_iam        = distinct(concat(local.s3_task_execution_bucket_arns, [for bucket in local.s3_task_execution_bucket_arns : format("%s/*", bucket)]))
   s3_task_role_bucket_arns                 = [for bucket in var.s3_task_buckets : format("arn:aws:s3:::%s", bucket)]
-  s3_task_role_bucket_arns_iam             = coalescelist(local.s3_task_role_bucket_arns, [for arn in local.s3_task_role_bucket_arns : format("%s/*", arn)])
+  s3_task_role_bucket_arns_iam             = try(coalescelist(local.s3_task_role_bucket_arns, [for arn in local.s3_task_role_bucket_arns : format("%s/*", arn)]), [])
 }
