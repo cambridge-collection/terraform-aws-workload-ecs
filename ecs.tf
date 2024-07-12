56cf7816d300060eab9ebfc49e4e90050b79dac5
Rory McHugh <rmm98@cam.ac.uk>
2024-07-12 15:59:01 +0100

    fix(ecs): Add condition to container_port argument of service_registries in aws_ecs_service.this

--- ecs.tf
+++ ecs.tf
@@ -74,7 +74,7 @@ resource "aws_ecs_service" "this" {
     content {
       registry_arn   = aws_service_discovery_service.this.0.arn
       container_name = var.ecs_service_container_name
-      container_port = var.ecs_service_container_port
+      container_port = var.ecs_network_mode == "awsvpc" ? null : var.ecs_service_container_port
     }
   }
 }
