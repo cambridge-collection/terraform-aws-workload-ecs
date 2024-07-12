ef765e2df50c53422210ea4c658379b78217ca2e
Rory McHugh <rmm98@cam.ac.uk>
2024-07-12 15:42:36 +0100

    fix(asg): Add count to aws_autoscaling_attachment resource based on ecs_network_mode input

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -41,6 +41,8 @@ resource "aws_lb_target_group" "this" {
 
 # Automatically register autoscaling group instances with load balancer
 resource "aws_autoscaling_attachment" "automatic_attachment" {
+  count =  var.ecs_network_mode == "awsvpc" ? 0 : 1 # NOTE autoscaling attachments only support instance targets
+
   autoscaling_group_name = var.asg_name
   lb_target_group_arn    = aws_lb_target_group.this.arn
 }
