70294ebe2ba72b8a94f8ef48d66f86e31aedbc94
Rory McHugh <rmm98@cam.ac.uk>
2024-08-08 11:34:12 +0100

    feat(eventbridge): Add log group and log group policy
    - Add aws_cloudwatch_log_group.ecs_stopped_tasks and
      aws_cloudwatch_log_resource_policy.ecs_stopped_tasks

--- eventbridge.tf
+++ eventbridge.tf
@@ -3,7 +3,7 @@ resource "aws_cloudwatch_event_rule" "ecs_stopped_tasks" {
   description = "Triggered when an Amazon ECS Task is stopped"
 
   event_pattern = jsonencode({
-    source = ["aws.ecs"]
+    source      = ["aws.ecs"]
     detail-type = ["ECS Task State Change"]
     detail = {
       desiredStatus = ["STOPPED"]
@@ -15,5 +15,38 @@ resource "aws_cloudwatch_event_rule" "ecs_stopped_tasks" {
 resource "aws_cloudwatch_event_target" "cloud_watch" {
   rule      = aws_cloudwatch_event_rule.ecs_stopped_tasks.name
   target_id = "ECSStoppedTasks"
-  arn       = var.cloudwatch_log_group_arn
+  arn       = aws_cloudwatch_log_group.ecs_stopped_tasks.arn
+}
+
+resource "aws_cloudwatch_log_group" "ecs_stopped_tasks" {
+  name = "/ecs/ECSStoppedTasksEvent"
+}
+
+resource "aws_cloudwatch_log_resource_policy" "ecs_stopped_tasks" {
+  policy_document = data.aws_iam_policy_document.create_log_events.json
+  policy_name     = "ecs-stopped-tasks-policy"
+}
+
+data "aws_iam_policy_document" "create_log_events" {
+  statement {
+    sid    = "LogEventsPolicy"
+    effect = "Allow"
+    principals {
+      type = "Service"
+      identifiers = [
+        "delivery.logs.amazonaws.com",
+        "events.amazonaws.com"
+      ]
+    }
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = [aws_cloudwatch_log_group.ecs_stopped_tasks.arn]
+    condition {
+      test     = "ArnEquals"
+      values   = [aws_cloudwatch_event_rule.ecs_stopped_tasks.arn]
+      variable = "aws:SourceArn"
+    }
+  }
 }
