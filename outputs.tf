98c94fa3577e5887f38e93064601788b83844be5
Rory McHugh <rmm98@cam.ac.uk>
2025-06-27 14:16:05 +0100

    feat(alb): Allow multiple target groups to connect to the service
    - Allow multiple aws_lb_target_group resources based on number of ports
    - Update load_balancer block on aws_ecs_service.this to refer to
      aws_lb_target_group.this
    - Replace aws_security_group_rule resources with
      aws_vpc_security_group_egress_rule and
      aws_vpc_security_group_ingress_rule resources
    - Update security group rules to refer to multiple ports
    - Add Name tags to security group rules
    - Replace output private_access_port with private_access_ports
    - Update README.md
    
    BREAKING CHANGES: Remove ecs_service_container_port input variable,
    and replace alb_target_group_port with alb_target_group_ports

--- outputs.tf
+++ outputs.tf
@@ -23,18 +23,18 @@ output "private_access_host" {
   description = "Route 53 record name for the A record created by Cloud Map Service Discovery"
 }
 
-output "private_access_port" {
-  value       = var.allow_private_access ? tostring(var.ecs_service_container_port) : ""
-  description = "Port number for accessing service via private access host name"
+output "private_access_ports" {
+  value       = var.allow_private_access ? [for port in var.alb_target_group_ports : tostring(port)] : []
+  description = "Port numbers for accessing service via private access host name"
 }
 
-output "alb_target_group_arn" {
-  value       = var.allow_public_access ? aws_lb_target_group.this.0.arn : ""
+output "alb_target_group_arns" {
+  value       = var.allow_public_access ? aws_lb_target_group.this.*.arn : []
   description = "ARN of the Load Balancer Target Group"
 }
 
-output "alb_target_group_arn_suffix" {
-  value       = var.allow_public_access ? aws_lb_target_group.this.0.arn_suffix : ""
+output "alb_target_group_arn_suffixes" {
+  value       = var.allow_public_access ? aws_lb_target_group.this.*.arn_suffix : []
   description = "ARN suffix of the Target Group for use with CloudWatch Metrics"
 }
 
@@ -59,7 +59,7 @@ output "ecs_service_name" {
 }
 
 output "cloudfront_distribution_id" {
-  value       = var.cloudfront_distribution_create && var.allow_public_access ? aws_cloudfront_distribution.this.0.id : ""
+  value       = var.cloudfront_distribution_create ? aws_cloudfront_distribution.this.0.id : ""
   description = "ID of the CloudFront Distribution"
 }
 
--- security_groups.tf
+++ security_groups.tf
@@ -2,42 +2,51 @@ data "aws_vpc" "this" {
   id = var.vpc_id
 }
 
-resource "aws_security_group_rule" "alb_egress_to_asg" {
-  count = var.allow_public_access ? 1 : 0
+resource "aws_vpc_security_group_egress_rule" "alb_egress_to_asg" {
+  count = var.allow_public_access ? length(var.alb_target_group_ports) : 0
 
-  type                     = "egress"
-  protocol                 = "tcp"
-  description              = "ALB Egress on port ${var.alb_target_group_port} for ${var.name_prefix}"
-  security_group_id        = var.alb_security_group_id
-  source_security_group_id = var.asg_security_group_id
-  from_port                = var.alb_target_group_port
-  to_port                  = var.alb_target_group_port
+  ip_protocol                  = "tcp"
+  description                  = "ALB Egress on port ${var.alb_target_group_ports[count.index]} for ${var.name_prefix}"
+  security_group_id            = var.alb_security_group_id
+  referenced_security_group_id = var.asg_security_group_id
+  from_port                    = var.alb_target_group_ports[count.index]
+  to_port                      = var.alb_target_group_ports[count.index]
+
+  tags = {
+    Name = format("alb-egress-to-asg-%s", var.alb_target_group_ports[count.index])
+  }
 }
 
-resource "aws_security_group_rule" "asg_ingress_from_alb" {
-  count = var.allow_public_access ? 1 : 0
+resource "aws_vpc_security_group_ingress_rule" "asg_ingress_from_alb" {
+  count = var.allow_public_access ? length(var.alb_target_group_ports) : 0
 
-  type                     = "ingress"
-  protocol                 = "tcp"
-  description              = "ASG Ingress on port ${var.alb_target_group_port} for ${var.name_prefix}"
-  security_group_id        = var.asg_security_group_id
-  source_security_group_id = var.alb_security_group_id
-  from_port                = var.alb_target_group_port
-  to_port                  = var.alb_target_group_port
+  ip_protocol                  = "tcp"
+  description                  = "ASG Ingress on port ${var.alb_target_group_ports[count.index]} for ${var.name_prefix}"
+  security_group_id            = var.asg_security_group_id
+  referenced_security_group_id = var.alb_security_group_id
+  from_port                    = var.alb_target_group_ports[count.index]
+  to_port                      = var.alb_target_group_ports[count.index]
+
+  tags = {
+    Name = format("asg-ingress-from-alb-%s", var.alb_target_group_ports[count.index])
+  }
 }
 
 # NOTE this may be needed where the security group owned by an external client
 # (var.ingress_security_group_id) has restricted outbound rules
-resource "aws_security_group_rule" "private_access_egress" {
-  count = var.allow_private_access && var.update_asg_security_group_to_access_service ? 1 : 0
+resource "aws_vpc_security_group_egress_rule" "private_access_egress" {
+  count = var.allow_private_access && var.update_asg_security_group_to_access_service ? length(var.alb_target_group_ports) : 0
 
-  type                     = "egress"
-  protocol                 = "tcp"
-  description              = "Egress on port ${var.alb_target_group_port} to ${var.ingress_security_group_id}"
-  security_group_id        = var.asg_security_group_id
-  source_security_group_id = var.ingress_security_group_id
-  from_port                = var.alb_target_group_port
-  to_port                  = var.alb_target_group_port
+  ip_protocol                  = "tcp"
+  description                  = "Egress on port ${var.alb_target_group_ports[count.index]} to ${var.ingress_security_group_id}"
+  security_group_id            = var.asg_security_group_id
+  referenced_security_group_id = var.ingress_security_group_id
+  from_port                    = var.alb_target_group_ports[count.index]
+  to_port                      = var.alb_target_group_ports[count.index]
+
+  tags = {
+    Name = format("asg-egress-private-access-%s", var.alb_target_group_ports[count.index])
+  }
 }
 
 resource "aws_security_group" "efs" {
@@ -52,71 +61,86 @@ resource "aws_security_group" "efs" {
   }
 }
 
-resource "aws_security_group_rule" "efs_ingress_nfs_from_asg" {
+resource "aws_vpc_security_group_ingress_rule" "efs_ingress_nfs_from_asg" {
   count = var.efs_create_file_system ? 1 : 0
 
-  type                     = "ingress"
-  protocol                 = "tcp"
-  description              = "EFS Ingress on port ${var.efs_nfs_mount_port} for ${var.name_prefix}"
-  security_group_id        = aws_security_group.efs.0.id
-  source_security_group_id = var.asg_security_group_id
-  from_port                = var.efs_nfs_mount_port
-  to_port                  = var.efs_nfs_mount_port
+  ip_protocol                  = "tcp"
+  description                  = "EFS Ingress on port ${var.efs_nfs_mount_port} for ${var.name_prefix}"
+  security_group_id            = aws_security_group.efs.0.id
+  referenced_security_group_id = var.asg_security_group_id
+  from_port                    = var.efs_nfs_mount_port
+  to_port                      = var.efs_nfs_mount_port
+
+  tags = {
+    Name = format("efs-ingress-from-asg-%s", var.efs_nfs_mount_port)
+  }
 }
 
-resource "aws_security_group_rule" "efs_ingress_nfs_from_vpc" {
+resource "aws_vpc_security_group_ingress_rule" "efs_ingress_nfs_from_vpc" {
   count = var.efs_create_file_system && var.datasync_s3_objects_to_efs ? 1 : 0
 
-  type              = "ingress"
-  protocol          = "tcp"
+  ip_protocol       = "tcp"
   description       = "EFS Ingress on port ${var.efs_nfs_mount_port} for ${var.name_prefix}"
   security_group_id = aws_security_group.efs.0.id
-  cidr_blocks       = [data.aws_vpc.this.cidr_block]
-  ipv6_cidr_blocks  = data.aws_vpc.this.ipv6_cidr_block != "" ? [data.aws_vpc.this.ipv6_cidr_block] : []
+  cidr_ipv4         = data.aws_vpc.this.cidr_block
+  cidr_ipv6         = data.aws_vpc.this.ipv6_cidr_block != "" ? data.aws_vpc.this.ipv6_cidr_block : null
   from_port         = var.efs_nfs_mount_port
   to_port           = var.efs_nfs_mount_port
+
+  tags = {
+    Name = format("efs-ingress-from-vpc-%s", var.efs_nfs_mount_port)
+  }
 }
 
-resource "aws_security_group_rule" "efs_egress_nfs_to_vpc" {
+resource "aws_vpc_security_group_egress_rule" "efs_egress_nfs_to_vpc" {
   count = var.efs_create_file_system && var.datasync_s3_objects_to_efs ? 1 : 0
 
-  type              = "egress"
-  protocol          = "tcp"
+  ip_protocol       = "tcp"
   description       = "EFS Egress on port ${var.efs_nfs_mount_port} for ${var.name_prefix}"
   security_group_id = aws_security_group.efs.0.id
-  cidr_blocks       = [data.aws_vpc.this.cidr_block]
-  ipv6_cidr_blocks  = data.aws_vpc.this.ipv6_cidr_block != "" ? [data.aws_vpc.this.ipv6_cidr_block] : []
+  cidr_ipv4         = data.aws_vpc.this.cidr_block
+  cidr_ipv6         = data.aws_vpc.this.ipv6_cidr_block != "" ? data.aws_vpc.this.ipv6_cidr_block : null
   from_port         = var.efs_nfs_mount_port
   to_port           = var.efs_nfs_mount_port
+
+  tags = {
+    Name = format("efs-egress-to-vpc-%s", var.efs_nfs_mount_port)
+  }
 }
 
-resource "aws_security_group_rule" "asg_egress_nfs_to_efs" {
+resource "aws_vpc_security_group_egress_rule" "asg_egress_nfs_to_efs" {
   count = var.efs_create_file_system ? 1 : 0
 
-  type                     = "egress"
-  protocol                 = "tcp"
-  description              = "ASG Egress on port ${var.efs_nfs_mount_port} for ${var.name_prefix}"
-  security_group_id        = var.asg_security_group_id
-  source_security_group_id = aws_security_group.efs.0.id
-  from_port                = var.efs_nfs_mount_port
-  to_port                  = var.efs_nfs_mount_port
+  ip_protocol                  = "tcp"
+  description                  = "ASG Egress on port ${var.efs_nfs_mount_port} for ${var.name_prefix}"
+  security_group_id            = var.asg_security_group_id
+  referenced_security_group_id = aws_security_group.efs.0.id
+  from_port                    = var.efs_nfs_mount_port
+  to_port                      = var.efs_nfs_mount_port
+
+  tags = {
+    Name = format("asg-egress-to-efs-%s", var.efs_nfs_mount_port)
+  }
 }
 
-resource "aws_security_group_rule" "asg_egress_nfs_to_existing_efs" {
+resource "aws_vpc_security_group_egress_rule" "asg_egress_nfs_to_existing_efs" {
   count = var.efs_use_existing_filesystem ? 1 : 0
 
-  type                     = "egress"
-  protocol                 = "tcp"
-  description              = "ASG Egress on port ${var.efs_nfs_mount_port} for ${var.name_prefix}"
-  security_group_id        = var.asg_security_group_id
-  source_security_group_id = var.efs_security_group_id
-  from_port                = var.efs_nfs_mount_port
-  to_port                  = var.efs_nfs_mount_port
+  ip_protocol                  = "tcp"
+  description                  = "ASG Egress on port ${var.efs_nfs_mount_port} for ${var.name_prefix}"
+  security_group_id            = var.asg_security_group_id
+  referenced_security_group_id = var.efs_security_group_id
+  from_port                    = var.efs_nfs_mount_port
+  to_port                      = var.efs_nfs_mount_port
 
   lifecycle {
     precondition {
       condition     = var.efs_security_group_id != null
       error_message = "To use an existing filesystem the input efs_security_group_id must be provided."
     }
+  }
+
+  tags = {
+    Name = format("asg-egress-to-efs-%s", var.efs_nfs_mount_port)
   }
 }
